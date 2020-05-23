f78f5945de916dee42602bd7ea1f6689dc761d1f
Damien Meur <dmeur@emisys.com>
2020-05-23 05:31:52 +0200

    add buggy

--- buggy/scripts/build-ssg-prod.sh
+++ buggy/scripts/build-ssg-prod.sh
@@ -2,7 +2,7 @@
 echo "Showing running docker containers"
 docker ps
 echo "Connecting to express_prod and building it with Next.js"
-docker exec -ti $(docker ps -aqf "name=portfolio_express_prod") npm run export:prod
+docker exec -ti $(docker ps -aqf "name=portfolio_express_prod") npm run build:prod
 echo "Connecting to express_prod and exporting it with Next.js SSG"
 docker exec -ti $(docker ps -aqf "name=portfolio_express_prod") npm run export:prod
 echo "Process finished"
