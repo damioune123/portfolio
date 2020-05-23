beace6271d9b66261a1ce8fd1bab538e06fbf4dd
Damien Meur <dmeur@emisys.com>
2020-05-23 06:26:11 +0200

    add buggy

---
+++ buggy/scripts/migrate-prod-to-staging.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+STAGING_STORAGE_FOLDER=/staging
+PROD_STORAGE_FOLDER=/prod
+
+echo "Replacing staging storage with prod storage"
+rm -rf ${STAGING_STORAGE_FOLDER}/* && cp -r ${PROD_STORAGE_FOLDER}/* ${STAGING_STORAGE_FOLDER}/
+echo "Connecting to mariadb and run the migration"
+docker exec -ti $(docker ps -aqf "name=mariadb") bash -c 'mysqldump  -u root -p${MYSQL_ROOT_PASSWORD} --opt directus-prod | mysql -u root -p${MYSQL_ROOT_PASSWORD} -C directus-staging'
